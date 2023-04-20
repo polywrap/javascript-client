4f321f72057c2b582a8a497a8ef5f92a50945846
Jure Bogunovic <bogunovij@gmail.com>
2023-04-20 22:49:39 +0200

    added subinvoke resolver extension tests

--- packages/uri-resolver-extensions/src/__tests__/resolver-extensions.spec.ts
+++ packages/uri-resolver-extensions/src/__tests__/resolver-extensions.spec.ts
@@ -1,7 +1,7 @@
 import { ExtendableUriResolver } from "../ExtendableUriResolver";
 import { expectHistory } from "./helpers/expectHistory";
 
-import { Uri, UriMap, UriResolutionContext, IWrapPackage } from "@polywrap/core-js";
+import { Uri, UriMap, UriResolutionContext, IWrapPackage, IUriResolutionContext } from "@polywrap/core-js";
 import { PolywrapCoreClient } from "@polywrap/core-client-js";
 import { PluginPackage } from "@polywrap/plugin-js";
 import { WasmPackage } from "@polywrap/wasm-js";
@@ -12,11 +12,12 @@ import fs from "fs";
 
 jest.setTimeout(600000);
 
+const subinvokeResolverUri = Uri.from("wrap://package/subinvoke-resolver");
 const customPluginResolverUri = Uri.from("wrap://package/test-resolver");
 const customPluginResolver = PluginPackage.from(() => ({
   tryResolveUri: async (
     args: any,
-    client: PolywrapCoreClient
+    client: PolywrapCoreClient,
   ): Promise<{
     uri?: string | null;
     manifest?: Uint8Array | null;
@@ -42,28 +43,75 @@ const customPluginResolver = PluginPackage.from(() => ({
   },
 }));
 
+const subinvokePluginResolver = PluginPackage.from(() => ({
+  tryResolveUri: async (
+    args: any,
+    client: PolywrapCoreClient,
+    _env: any,
+    resolutionContext?: IUriResolutionContext,
+  ): Promise<{
+    uri?: string | null;
+    manifest?: Uint8Array | null;
+  } | null> => {
+    const result = await client.invoke<{
+      uri?: string | null;
+      manifest?: Uint8Array | null;
+    } | null>({ 
+      uri: Uri.from("wrap://package/test-resolver"),
+      method: "tryResolveUri",
+      args,
+      resolutionContext
+    });
+
+    if (!result.ok) {
+      throw result.error;
+    }
+
+    return result.value;
+  },
+}));
+
 describe("Resolver extensions", () => {
 
   let testResolverPackage: IWrapPackage;
+  let subinvokeResolverPackage: IWrapPackage;
 
   beforeAll(async () => {
-    const wrapDir = path.join(__dirname, "/wrappers/test-resolver");
+    const testResolverDir = path.join(__dirname, "/wrappers/test-resolver");
 
     // Build the test-resolver wrapper
     const res = await Commands.build({}, {
-      cwd: wrapDir
+      cwd: testResolverDir
     });
 
     if (res.exitCode !== 0) {
       fail(`STDOUT: ${res.stdout}\nSTDERR: ${res.stderr}`);
     }
 
-    const wrapBuildDir = path.join(wrapDir, "build");
+    const wrapBuildDir = path.join(testResolverDir, "build");
 
     // Load the wrapper from disk
     testResolverPackage = WasmPackage.from(
       fs.readFileSync(path.join(wrapBuildDir, "wrap.info")),
       fs.readFileSync(path.join(wrapBuildDir, "wrap.wasm"))
+    );
+
+    const subinvokeResolverDir = path.join(__dirname, "/wrappers/subinvoke-resolver");
+
+    // Build the test-resolver wrapper
+    const subinvokeRes = await Commands.build({}, {
+      cwd: subinvokeResolverDir
+    });
+
+    if (subinvokeRes.exitCode !== 0) {
+      fail(`STDOUT: ${res.stdout}\nSTDERR: ${res.stderr}`);
+    }
+
+    const subinvokeBuildDir = path.join(subinvokeResolverDir, "build");
+    // Load the wrapper from disk
+    subinvokeResolverPackage = WasmPackage.from(
+      fs.readFileSync(path.join(subinvokeBuildDir, "wrap.info")),
+      fs.readFileSync(path.join(subinvokeBuildDir, "wrap.wasm"))
     );
   });
 
@@ -111,6 +159,54 @@ describe("Resolver extensions", () => {
     expect(result.value.uri.uri).toEqual(redirectedUri.uri);
   });
 
+  it("can resolve URI with plugin extension and subinvoke", async () => {
+    const sourceUri = Uri.from(`test/from`);
+    const redirectedUri = Uri.from("test/to");
+
+    const client = new PolywrapCoreClient({
+      interfaces: new UriMap<Uri[]>([
+        [
+          ExtendableUriResolver.defaultExtInterfaceUris[0],
+          [subinvokeResolverUri],
+        ],
+      ]),
+      resolver: RecursiveResolver.from([
+        StaticResolver.from([
+          {
+            uri: customPluginResolverUri,
+            package: customPluginResolver,
+          },
+          {
+            uri: subinvokeResolverUri,
+            package: subinvokePluginResolver,
+          },
+        ]),
+        new ExtendableUriResolver(),
+      ]),
+    });
+
+    let resolutionContext = new UriResolutionContext();
+    let result = await client.tryResolveUri({
+      uri: sourceUri,
+      resolutionContext,
+    });
+
+    await expectHistory(
+      resolutionContext.getHistory(),
+      "can-resolve-uri-with-subinvoke"
+    );
+
+    if (!result.ok) {
+      fail(result.error);
+    }
+
+    if (result.value.type !== "uri") {
+      fail("Expected a URI, received: " + result.value.type);
+    }
+
+    expect(result.value.uri.uri).toEqual(redirectedUri.uri);
+  });
+
   it("can resolve a package with plugin extension", async () => {
     const sourceUri = Uri.from(`test/package`);
     const redirectedUri = Uri.from("test/package");
@@ -142,6 +238,54 @@ describe("Resolver extensions", () => {
     await expectHistory(
       resolutionContext.getHistory(),
       "can-resolve-package"
+    );
+
+    if (!result.ok) {
+      fail(result.error);
+    }
+
+    if (result.value.type !== "package") {
+      fail("Expected a package, received: " + result.value.type);
+    }
+
+    expect(result.value.uri.uri).toEqual(redirectedUri.uri);
+  });
+
+  it("can resolve package with plugin extension and subinvoke", async () => {
+    const sourceUri = Uri.from(`test/package`);
+    const redirectedUri = Uri.from("test/package");
+
+    const client = new PolywrapCoreClient({
+      interfaces: new UriMap<Uri[]>([
+        [
+          ExtendableUriResolver.defaultExtInterfaceUris[0],
+          [subinvokeResolverUri],
+        ],
+      ]),
+      resolver: RecursiveResolver.from([
+        StaticResolver.from([
+          {
+            uri: customPluginResolverUri,
+            package: customPluginResolver,
+          },
+          {
+            uri: subinvokeResolverUri,
+            package: subinvokePluginResolver,
+          },
+        ]),
+        new ExtendableUriResolver(),
+      ]),
+    });
+
+    let resolutionContext = new UriResolutionContext();
+    let result = await client.tryResolveUri({
+      uri: sourceUri,
+      resolutionContext,
+    });
+
+    await expectHistory(
+      resolutionContext.getHistory(),
+      "can-resolve-package-with-subinvoke"
     );
 
     if (!result.ok) {
@@ -203,6 +347,66 @@ args: {
     );
   });
 
+  it("shows the plugin resolver extension error with subinvoke", async () => {
+    const sourceUri = Uri.from(`test/error`);
+
+    const client = new PolywrapCoreClient({
+      interfaces: new UriMap<Uri[]>([
+        [
+          ExtendableUriResolver.defaultExtInterfaceUris[0],
+          [subinvokeResolverUri],
+        ],
+      ]),
+      resolver: RecursiveResolver.from([
+        StaticResolver.from([
+          {
+            uri: customPluginResolverUri,
+            package: customPluginResolver,
+          },
+          {
+            uri: subinvokeResolverUri,
+            package: subinvokePluginResolver,
+          },
+        ]),
+        new ExtendableUriResolver(),
+      ]),
+    });
+
+    let resolutionContext = new UriResolutionContext();
+    let result = await client.tryResolveUri({
+      uri: sourceUri,
+      resolutionContext,
+    });
+
+    await expectHistory(
+      resolutionContext.getHistory(),
+      "shows-plugin-extension-error-with-subinvoke"
+    );
+
+    if (result.ok) {
+      fail("Expected an error, received: " + result.value.type);
+    }
+
+    expect((result.error as Error)?.message).toEqual(
+      `Test error
+code: 51 WRAPPER INVOKE ABORTED
+uri: wrap://package/test-resolver
+method: tryResolveUri
+args: {
+  "authority": "test",
+  "path": "error"
+} 
+code: 51 WRAPPER INVOKE ABORTED
+uri: wrap://package/subinvoke-resolver
+method: tryResolveUri
+args: {
+  "authority": "test",
+  "path": "error"
+} 
+source: { file: "/home/nerfzael/dev/web3api/repos/javascript-client/packages/plugin/src/PluginWrapper.ts", row: 105, col: 21 }`
+    );
+  });
+
   it("does not resolve a URI when not a match with plugin extension", async () => {
     const uri = new Uri("test/not-a-match");
 
@@ -242,6 +446,51 @@ args: {
 
     expect(result.value.uri.uri).toEqual("wrap://test/not-a-match");
   });
+
+  it("does not resolve a URI when not a match with plugin extension and subinvoke", async () => {
+    const uri = new Uri("test/not-a-match");
+
+    const client = new PolywrapCoreClient({
+      interfaces: new UriMap<Uri[]>([
+        [
+          ExtendableUriResolver.defaultExtInterfaceUris[0],
+          [subinvokeResolverUri],
+        ],
+      ]),
+      resolver: RecursiveResolver.from([
+        StaticResolver.from([
+          {
+            uri: customPluginResolverUri,
+            package: customPluginResolver,
+          },
+          {
+            uri: subinvokeResolverUri,
+            package: subinvokePluginResolver,
+          },
+        ]),
+        new ExtendableUriResolver(),
+      ]),
+    });
+
+    const resolutionContext = new UriResolutionContext();
+    const result = await client.tryResolveUri({ uri, resolutionContext });
+
+    await expectHistory(
+      resolutionContext.getHistory(),
+      "not-a-match-with-subinvoke"
+    );
+
+    if (!result.ok) {
+      fail(result.error);
+    }
+
+    if (result.value.type !== "uri") {
+      fail("Expected a uri, received: " + result.value.type);
+    }
+
+    expect(result.value.uri.uri).toEqual("wrap://test/not-a-match");
+  });
+
 
   it("can resolve URI with wasm extension", async () => {
     const sourceUri = Uri.from(`test/from`);
@@ -287,6 +536,55 @@ args: {
     expect(result.value.uri.uri).toEqual(redirectedUri.uri);
   });
 
+  it("can resolve URI with wasm extension and subinvoke", async () => {
+    const sourceUri = Uri.from(`test/from`);
+    const redirectedUri = Uri.from("test/to");
+
+    const subinvokeResolverUri = Uri.from("wrap://package/subinvoke-resolver");
+    const client = new PolywrapCoreClient({
+      interfaces: new UriMap<Uri[]>([
+        [
+          ExtendableUriResolver.defaultExtInterfaceUris[0],
+          [subinvokeResolverUri],
+        ],
+      ]),
+      resolver: RecursiveResolver.from([
+        StaticResolver.from([
+          {
+            uri: customPluginResolverUri,
+            package: testResolverPackage,
+          },
+          {
+            uri: subinvokeResolverUri,
+            package: subinvokeResolverPackage,
+          },
+        ]),
+        new ExtendableUriResolver(),
+      ]),
+    });
+
+    let resolutionContext = new UriResolutionContext();
+    let result = await client.tryResolveUri({
+      uri: sourceUri,
+      resolutionContext,
+    });
+
+    await expectHistory(
+      resolutionContext.getHistory(),
+      "can-resolve-uri-with-subinvoke"
+    );
+
+    if (!result.ok) {
+      fail(result.error);
+    }
+
+    if (result.value.type !== "uri") {
+      fail("Expected a URI, received: " + result.value.type);
+    }
+
+    expect(result.value.uri.uri).toEqual(redirectedUri.uri);
+  });
+
   it("can resolve a package with wasm extension", async () => {
     const sourceUri = Uri.from(`test/package`);
     const redirectedUri = Uri.from("test/package");
@@ -318,6 +616,54 @@ args: {
     await expectHistory(
       resolutionContext.getHistory(),
       "can-resolve-package"
+    );
+
+    if (!result.ok) {
+      fail(result.error);
+    }
+
+    if (result.value.type !== "package") {
+      fail("Expected a package, received: " + result.value.type);
+    }
+
+    expect(result.value.uri.uri).toEqual(redirectedUri.uri);
+  });
+
+  it("can resolve a package with wasm extension and subinvoke", async () => {
+    const sourceUri = Uri.from(`test/package`);
+    const redirectedUri = Uri.from("test/package");
+
+    const client = new PolywrapCoreClient({
+      interfaces: new UriMap<Uri[]>([
+        [
+          ExtendableUriResolver.defaultExtInterfaceUris[0],
+          [subinvokeResolverUri],
+        ],
+      ]),
+      resolver: RecursiveResolver.from([
+        StaticResolver.from([
+          {
+            uri: customPluginResolverUri,
+            package: testResolverPackage,
+          },
+          {
+            uri: subinvokeResolverUri,
+            package: subinvokeResolverPackage,
+          },
+        ]),
+        new ExtendableUriResolver(),
+      ]),
+    });
+
+    let resolutionContext = new UriResolutionContext();
+    let result = await client.tryResolveUri({
+      uri: sourceUri,
+      resolutionContext,
+    });
+
+    await expectHistory(
+      resolutionContext.getHistory(),
+      "can-resolve-package-with-subinvoke"
     );
 
     if (!result.ok) {
@@ -380,6 +726,70 @@ source: { file: "src/wrap/module/wrapped.rs", row: 35, col: 21 }`
     );
   });
 
+  it("shows the wasm resolver extension error with subinvoke", async () => {
+    const sourceUri = Uri.from(`test/error`);
+
+    const client = new PolywrapCoreClient({
+      interfaces: new UriMap<Uri[]>([
+        [
+          ExtendableUriResolver.defaultExtInterfaceUris[0],
+          [subinvokeResolverUri],
+        ],
+      ]),
+      resolver: RecursiveResolver.from([
+        StaticResolver.from([
+          {
+            uri: customPluginResolverUri,
+            package: testResolverPackage,
+          },
+          {
+            uri: subinvokeResolverUri,
+            package: subinvokeResolverPackage,
+          },
+        ]),
+        new ExtendableUriResolver(),
+      ]),
+    });
+
+    let resolutionContext = new UriResolutionContext();
+    let result = await client.tryResolveUri({
+      uri: sourceUri,
+      resolutionContext,
+    });
+
+    await expectHistory(
+      resolutionContext.getHistory(),
+      "shows-wasm-extension-error-with-subinvoke"
+    );
+
+    if (result.ok) {
+      fail("Expected an error, received: " + result.value.type);
+    }
+
+    expect((result.error as Error)?.message).toEqual(
+      `SubInvocation exception encountered
+code: 51 WRAPPER INVOKE ABORTED
+uri: wrap://package/subinvoke-resolver
+method: tryResolveUri
+args: {
+  "authority": "test",
+  "path": "error"
+} 
+source: { file: "src/wrap/module/wrapped.rs", row: 35, col: 21 }
+
+Another exception was encountered during execution:
+WrapError: __wrap_abort: Test error
+code: 51 WRAPPER INVOKE ABORTED
+uri: wrap://package/test-resolver
+method: tryResolveUri
+args: {
+  "authority": "test",
+  "path": "error"
+} 
+source: { file: "src/wrap/module/wrapped.rs", row: 35, col: 21 }`
+    );
+  });
+
   it("does not resolve a URI when not a match with wasm extension", async () => {
     const uri = new Uri("test/not-a-match");
 
@@ -407,6 +817,50 @@ source: { file: "src/wrap/module/wrapped.rs", row: 35, col: 21 }`
     await expectHistory(
       resolutionContext.getHistory(),
       "not-a-match"
+    );
+
+    if (!result.ok) {
+      fail(result.error);
+    }
+
+    if (result.value.type !== "uri") {
+      fail("Expected a uri, received: " + result.value.type);
+    }
+
+    expect(result.value.uri.uri).toEqual("wrap://test/not-a-match");
+  });
+
+  it("does not resolve a URI when not a match with wasm extension and subinvoke", async () => {
+    const uri = new Uri("test/not-a-match");
+
+    const client = new PolywrapCoreClient({
+      interfaces: new UriMap<Uri[]>([
+        [
+          ExtendableUriResolver.defaultExtInterfaceUris[0],
+          [subinvokeResolverUri],
+        ],
+      ]),
+      resolver: RecursiveResolver.from([
+        StaticResolver.from([
+          {
+            uri: customPluginResolverUri,
+            package: testResolverPackage,
+          },
+          {
+            uri: subinvokeResolverUri,
+            package: subinvokeResolverPackage,
+          },
+        ]),
+        new ExtendableUriResolver(),
+      ]),
+    });
+
+    const resolutionContext = new UriResolutionContext();
+    const result = await client.tryResolveUri({ uri, resolutionContext });
+
+    await expectHistory(
+      resolutionContext.getHistory(),
+      "not-a-match-with-subinvoke"
     );
 
     if (!result.ok) {
