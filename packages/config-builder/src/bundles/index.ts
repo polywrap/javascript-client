6570bb2d5689478b26592f034a5f3dfd109fc69a
dOrgJelli <jelli@dorg.tech>
2023-06-14 01:30:53 -0400

    chore: init config-bundle packages

--- packages/config-builder/src/bundles/index.ts
+++ packages/config-builder/src/bundles/index.ts
@@ -1,1 +1,48 @@
-export * as DefaultBundle from "./default";
+import { BuilderConfig } from "../types";
+
+export type BundleName =
+  | "sys"
+  | "sys-node"
+  | "sys-browser"
+  | "web"
+  | "web3";
+
+export const bundlePackages: Record<BundleName, string> = {
+  "sys": "@polywrap/sys-config-bundle-js",
+  "sys-node": "@polywrap/sys-node-config-bundle-js",
+  "sys-browser": "@polywrap/sys-browser-config-bundle-js",
+  "web": "@polywrap/web-config-bundle-js",
+  "web3": "@polywrap/web3-config-bundle-js",
+}
+
+export interface BundleModule {
+  getBundleConfig(): BuilderConfig;
+}
+
+export async function loadBundleConfig(name: BundleName): Promise<BuilderConfig> {
+  const packageName = bundlePackages[name];
+
+  if (!packageName) {
+    throw Error(
+      `Unknown bundle name "${name}". Supported bundle names: ${
+        Object.keys(bundlePackages).join(", ")
+      }`
+    );
+  }
+
+  const bundleModule: BundleModule = await import(packageName);
+
+  if (!bundleModule) {
+    throw Error(
+      `Bundle ${name} at ${packageName} cannot be found, is it installed? Try 'npm i ${packageName}'`
+    );
+  }
+
+  if (!bundleModule.getBundleConfig) {
+    throw Error(
+      `Bundle ${name} at ${packageName} is incompatible with the BundleModule interface, please ensure it uses the correct version of the client-config-builde package.`
+    );
+  }
+
+  return bundleModule.getBundleConfig();
+}
