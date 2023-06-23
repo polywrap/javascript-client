ae58016d2044a5e3a0ae279d03f2bf1a0da0a071
dOrgJelli <jelli@dorg.tech>
2023-06-23 13:00:09 -0400

    chore: changes based on feedback

--- integration-tests/react-app/src/App.tsx
+++ integration-tests/react-app/src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { PolywrapClient } from "@polywrap/client-js";
+import { bundle } from "@polywrap/sys-config-bundle-js";
 
 function App() {
   const client = new PolywrapClient();
+
+  // Make extra sure the sys bundle works in the browser
+  console.log(bundle);
 
   return (
     <div className="App">
